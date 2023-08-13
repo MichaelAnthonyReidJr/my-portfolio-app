489dd3fd62f11bebf81ca87e062d20a0d7b778c8
Michael Reid <EfficiencyMarriesEfficacy@gmail.com>
2023-08-13 09:04:20 -0400

    revise contact form to use emailjs

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,48 @@
 import "./FormStyles.css"
-import React from 'react'
+import React, { useState } from 'react'
+import emailjs from '@emailjs/browser'
 
 const Form = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  })
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    const emailData = {
+      to: "EfficiencyMarriesEfficacy@gmail.com",
+      from: formData.email,
+      subject: "Contact Form Submisiion",
+      text: formData.message,
+    };
+    emailjs.sendForm("service_9z66dz8", "template_py7fg2", e.target, emailData);
+  }; 
+
   return (
     <div className = "form">
-        <form>
-            <label>Your Name</label>
-            <input type ="text"></input>
-            <label>Email</label>
-            <input type ="text"></input>
-            <label>Subject</label>
-            <input type ="text"></input>
-            <label>Message</label>
-            <textarea rows ="7" placeholder="Type Your Message Here"/>
+        <form onSubmit = {sendEmail} >
+            <input 
+            type = "text"
+            name = "name"
+            placeholder = "Your name"
+            value = {formData.name}
+            onChange={(e) => setFormData({ ...formData, name:e.target.value })}
+            />
+            <input 
+            type = "email"
+            name = "email"
+            placeholder = "Your email"
+            value = {formData.email}
+            onChange={(e) => setFormData({ ...formData, email:e.target.value })}
+            />
+            <textarea rows = "7"
+            name = "message"
+            placeholder = "Your message"
+            value = {formData.message}
+            onChange={(e) => setFormData({ ...formData, message:e.target.value })}
+            />
             <button className = "btn">Submit</button>
         </form>
 
@@ -20,4 +50,4 @@ const Form = () => {
   )
 }
 
-export default Form
+export default Form;
